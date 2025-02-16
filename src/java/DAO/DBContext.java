edb1d9f032c64163b0489c57e54a0f46de78f40e
Tra Pham <phamtra001@gmail.com>
2025-02-16 21:05:14 +0700

    Fix defect ID 10

--- src/java/DAO/DBContext.java
+++ src/java/DAO/DBContext.java
@@ -1,29 +1,41 @@
 package DAO;
 
+import DAO.AppContextListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import jakarta.servlet.ServletContext;
 public class DBContext {
+    private static String user;
+    private static String pass;
     protected Connection connection;
+    
+    static {
+        ServletContext context = AppContextListener.getServletContext();
+        if (context != null) {
+            user = context.getInitParameter("DB_USER");
+            pass = context.getInitParameter("DB_PASS");
+        }
+    }   
     public DBContext()
     {
 
         try {
-            String user = "sa";
-            String pass = "123456";
             String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=PRJ301_Group3_BusSystem;encrypt=true;trustServerCertificate=true";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    public static void main(String[] args) {
+        System.out.println(user);
+        System.out.println(pass);
     }
 }
